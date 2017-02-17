6df202ef4e2ed04f766e725e06cdd13464e6c31b
bakasajoshua <baksajoshua09@gmail.com>
2017-02-17 08:49:41 +0300

    Removal of the edit

--- assets/customScripts/employmentHistory.js
+++ assets/customScripts/employmentHistory.js
@@ -174,7 +174,7 @@ $(document).ready(function(){
 			$table += "<td>"+$position+"</td>";
 			$table += "<td>"+$responsibilities+"</td>";
 			$table += "<td>"+$yearsCompleted+"</td>";
-			$table += '<td><a href="'+updateEmploymentURL+'/'+$email+'/'+$institution+'/'+$position+'"><button class="btn btn-default"><i class="fa fa-pencil-square-o" aria-hidden="true">Edit</i></button></a></td>';
+			// $table += '<td><a href="'+updateEmploymentURL+'/'+$email+'/'+$institution+'/'+$position+'"><button class="btn btn-default"><i class="fa fa-pencil-square-o" aria-hidden="true">Edit</i></button></a></td>';
 			$table += '<td><a href="'+deleteEmploymentURL+'/'+$email+'/'+$institution+'/'+$position+'"><button class="btn btn-default"><i class="fa fa-times" aria-hidden="true">Delete</i></button></a></td>';
 			$table += "</tr>";
 			$("#employmentForm").hide();
